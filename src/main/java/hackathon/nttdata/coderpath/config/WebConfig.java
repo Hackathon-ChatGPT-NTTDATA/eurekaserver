31819012c1a32646d3e6b7e014d8a5c81de1215a
joffre <joffre.hermosilla@gmail.com>
2023-06-07 19:50:11 -0500

    chore[4] configuracion nuevo logo

---
+++ src/main/java/hackathon/nttdata/coderpath/config/WebConfig.java
@@ -0,0 +1,20 @@
+package hackathon.nttdata.coderpath.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/eureka/images/spring-logo-eureka.*")
+				.addResourceLocations("classpath:/fotocreador/")
+				.setCachePeriod(0);
+
+		
+	}
+
+}
